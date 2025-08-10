b8f38c0c40368308058d010f69ec476288337bcf
Alexey Stupnikov <aleks.gugl.77@gmail.com>
2025-08-10 21:59:39 +0400

    initial commit

---
+++ src/components/app-button.js
@@ -0,0 +1,45 @@
+import { LitElement, html, css } from 'lit';
+
+export class AppButton extends LitElement {
+    static properties = {
+        label: { type: String },
+        disabled: { type: Boolean },
+    };
+
+    static styles = css`
+        button {
+            padding: 8px 16px;
+            font-size: 1rem;
+            border: none;
+            border-radius: 4px;
+            background-color: var(--gray);
+            color: var(--black);
+            cursor: pointer;
+        }
+  `;
+
+    constructor() {
+        super();
+        this.label = 'Button';
+        this.disabled = false;
+    }
+
+    _handleClick(e) {
+        this.dispatchEvent(new CustomEvent('button-click', {
+            detail: e,
+            bubbles: true,
+            composed: true,
+        }));
+    }
+
+
+    render() {
+        return html`
+            <button ?disabled=${this.disabled} @click=${this._handleClick}>
+                ${this.label}
+            </button>
+        `;
+    }
+}
+
+customElements.define('app-button', AppButton);
