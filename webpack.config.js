43aba1227bc643ce7e84f5b98ebdfcaba81451f0
Alexey Stupnikov <aleks.gugl.77@gmail.com>
2025-08-12 21:30:38 +0400

    dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,11 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         clean: true,
     },
     devServer: {
-        static: './dist',
+        static: './docs',
         port: 3000,
         hot: true,
     },
