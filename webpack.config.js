f2eae6835e773f7a2fb3931ea6309ab905d6bf90
Alexey Stupnikov <aleks.gugl.77@gmail.com>
2025-08-12 21:09:39 +0400

    webpack for build

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devServer: {
+        static: './dist',
+        port: 3000,
+        hot: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                { targets: 'defaults' }
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+};
