c2e5f0de22b611773e4a69079ae0f3944f29cb9d
Alexey Stupnikov <aleks.gugl.77@gmail.com>
2025-08-13 10:18:10 +0400

    reset js cache after build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, 'docs'),
         clean: true,
     },
